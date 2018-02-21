1f6426bd5e52601f05a097f7099c59ad9a8936d0
Alessandro Rodi <alrodi@gymever.com>
2018-02-21 13:15:15 -0500

    -1.8 language level support
    How it works?
    -Send in a .txt file of all java code file names (file1.java /n file2.java …) for implementation
    -Put .txt file and java code in /resources (using intelliJ)

--- src/main/java/com/soen691/Main.java
+++ src/main/java/com/soen691/Main.java
@@ -1,8 +1,58 @@
 package com.soen691;
 
+import StaticChecker.FileParser;
+import StaticChecker.StaticChecker;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        if (args == null || args.length == 0){
+            System.out.println("No arguments provided.");
+        }
+
+        for (String arg: args) {
+            ArrayList<String> javaFiles = parseTextFile(arg);
+            for (String javaFile : javaFiles) {
+                try{
+                    FileParser.parseFile(javaFile);
+
+                }catch(Exception e){
+                    System.out.println(e.getStackTrace());
+                }
+            }
+        }
     }
+
+    /**
+     * returns an array of java code names we need to analyze.
+     */
+    private static ArrayList<String> parseTextFile(String path){
+
+        ArrayList<String> javaFiles = new ArrayList<String>();
+
+        //read file into stream, try-with-resources
+        try (Stream<String> stream = Files.lines(Paths.get(path))) {
+
+            stream.forEach(line->{
+                javaFiles.add(line);
+            });
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (Exception e ){
+            System.out.println(e.getStackTrace());
+        }
+
+        return javaFiles;
+    }
+
 }
